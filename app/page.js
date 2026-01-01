6563a338a944c82689e1fa1c36332569dde0d3e1
Foolais <wahyu.esya17@gmail.com>
2026-01-01 09:01:53 +0700

    add title

--- app/page.js
+++ app/page.js
@@ -15,10 +15,16 @@ export default function Home() {
           className={styles.heroImage}
         />
         <Header />
-        <div>
+        <div className={styles.headingWrapper}>
           <h1 className={styles.headingTitle}>
             Make your comfort is our happiness
           </h1>
+          <p className={styles.headingSubtitle}>
+            Studio villa made of bamboo, located near the top of Mount Geulis
+            with a stunning 180 degree bird&apos;s eye view.
+          </p>
+          <div className={styles.headingMainButton}>Book Now</div>
+          <div className={styles.headingSecondaryButton}>Explore Rooms</div>
         </div>
       </div>
     </main>
